3af2c21949cd1b0d5e69114e108ea21184fd004c
Youhan Lalwani <137485805+Jotunn9025@users.noreply.github.com>
2025-03-07 00:20:50 +0530

    graphing

--- Linear_Regression/Test.py
+++ Linear_Regression/Test.py
@@ -1,12 +1,21 @@
 import numpy as np
 from Linear_Regression import LinearRegression
+import matplotlib.pyplot as plt
 
-X=np.arange(1,101,1)
+X=np.linspace(0,1000,1000)
 print(X)
-Y=X*20
 
-model=LinearRegression(learning_rate=1e-6,iterations=int(1e4))
-
+noise = np.random.normal(0, 0.2, size=X.shape)
+X=(X-X.min())/(X.max()-X.min())
+model=LinearRegression(learning_rate=1e-3,iterations=int(1e5))
+Y=10*X+20
+Y=Y+noise
 model.fit(X,Y)
 
-print(model.predict([10,100,1000,5]))
+y_pred=model.predict(X)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.scatter(X,Y,color="lightgray",s=1,label="Experimental")
+plt.plot(X,y_pred,color="red",label="predicted")
+plt.legend()
+plt.show()
